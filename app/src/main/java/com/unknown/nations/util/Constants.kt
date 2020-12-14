8531bf93b47f595e4f201e76e6a6bdc2f0eaec58
Adel Khalifa <48628168+adel-khalifa@users.noreply.github.com>
2020-12-14 22:28:14 +0200

    update kotlin ext

--- app/src/main/java/com/unknown/nations/util/Constants.kt
+++ app/src/main/java/com/unknown/nations/util/Constants.kt
@@ -5,9 +5,7 @@ class Constants {
         const val BASE_URL = "https://newsapi.org/"
         const val API_KEY = "a0b4079842044971b4eb02d179aaeaa5"
         const val ARTICLE_TABLE_NAME = "articles_table"
-        const val ARTICLE_CASHED_TABLE_NAME = "articles_cashed_table"
         const val ARTICLE_DATABASE_NAME ="article_database.db"
-        const val ARTICLE_SERIALIZABLE = "article"
 
     }
 }
