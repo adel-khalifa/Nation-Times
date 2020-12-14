c8b3ea985db0f2da32fe4e51a30060b8aa05ccb0
Adel Khalifa <48628168+adel-khalifa@users.noreply.github.com>
2020-12-14 23:19:04 +0200

    guide to add key

--- app/src/main/java/com/unknown/nations/network/RetrofitInstance.kt
+++ app/src/main/java/com/unknown/nations/network/RetrofitInstance.kt
@@ -15,6 +15,7 @@ class RetrofitInstance {
 
         private val retrofit by lazy {
             // using interceptor to inject a stable query parameter which is key
+            // make sure to update the (gradle.properties)'s API_KEY field
             val keyInterceptor  = Interceptor.invoke { chain ->
                 val url =chain.request().url.newBuilder().addQueryParameter("apiKey", BuildConfig.API_KEY).build()
                 val request = chain.request().newBuilder().url(url).build()
